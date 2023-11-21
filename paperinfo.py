6d56c8f6f148642a78ee028f5d679df701f7ee87
George Araújo <george.gcac@gmail.com>
2023-11-21 12:04:32 -0300

    Added arxiv_id field to paperinfo
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- paperinfo.py
+++ paperinfo.py
@@ -4,12 +4,13 @@ from dataclasses import dataclass, field
 @dataclass
 class PaperInfo:
     abstract_url: str
+    clean_title: str
     pdf_url: str
     title: str
-    clean_title: str
     # indicates the weight of the word in this paper, being
     # key: index of word
     # value: weight of the word, associated with occurrence in title and/or abstract
     abstract_freq: dict[int, float] = field(default_factory=dict)
+    arxiv_id: None | str = None
     conference: str = ''
     year: int = 0
